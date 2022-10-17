80c4b0bf2b28ac05bcdee52debbab4006a9fab7c
Srajan <srajandubey27@gmail.com>
2022-10-17 11:14:31 +0530

    commiting Modification1

---
+++ Modification1.java
@@ -0,0 +1,8 @@
+public class PracticeProject{
+     public static void main(String args[])
+     {
+        int a=2;
+        int b=5;
+        System.out.println("can you please print the value of "+a+b+a*b);
+     }
+}
